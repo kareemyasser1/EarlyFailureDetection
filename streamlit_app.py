9e6cc09eb1f713b3eb0cc9069391e65801cdc810
kareemyasser1 <136920255+kareemyasser1@users.noreply.github.com>
2025-05-26 03:27:35 +0300

    Add files via upload

---
+++ streamlit_app.py
@@ -0,0 +1,7 @@
+# This file is used by Streamlit Cloud for deployment
+# It imports the demo version which works without TensorFlow
+
+from app_demo import *
+
+if __name__ == "__main__":
+    main() 
